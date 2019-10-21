1c2012fcfd7e918b99baa0fbb11194516fe1ae74
Ishan Dutta <ali3nator5000@gmail.com>
2019-10-20 21:01:46 -0500

    Added spawn manager script for zombies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] zombiesPrefabs;
+
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 31.0f;
+    [SerializeField] private float startDelay = 5.0f;
+    [SerializeField] private float spawnInterval = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimals", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //randomly generate animal index and spawn location
+
+    }
+
+    public void SpawnRandomAnimals()
+    {
+        if (gameObject != null)
+        {
+            int animalIndex = Random.Range(0, zombiesPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+            Instantiate(zombiesPrefabs[animalIndex], spawnPos, zombiesPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
