2d467d81f6700014e7190278189c148e64e340cf
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-10-21 12:56:42 -0500

    Fixed Minor issues

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] GameObject Player;
+    public Transform Playerpos;
     int MoveSpeed = 4;
     int MaxDist = 10;
     int MinDist = 5;
 
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Player.transform);
+        transform.LookAt(Playerpos);
 
         //if (Vector3.Distance(transform.position, Player.transform.position) >= MinDist)
         //{
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        //transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        rb.AddForce(transform.forward * MoveSpeed);
 
             //if (Vector3.Distance(transform.position, Player.transform.position) <= MaxDist)
             //{
