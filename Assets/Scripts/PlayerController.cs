556ea4c369cfc58e0aa3a89ab4b4d38cf363b03e
Ishan Dutta <ali3nator5000@gmail.com>
2019-10-20 20:54:33 -0500

    Set player bounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Text scoreText;
     GameObject player;
 
     // Start is called before the first frame update
     public float speed = 20.0f;
-    public float turnSpeed = 20.0f;
+    public float turnSpeed = 200.0f;
     public float horizontalInput;
     public float forwardInput;
+
+    public GameObject projectileBullet;
+
+
 
     float score = 0f;
     public bool isOnGround = true;
@@ -28,11 +31,32 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        //moves the car forward basaed on vertical input
+        //Keeping the player in bound along the x axis
+        if (transform.position.x < -23)
+        {
+            transform.position = new Vector3(-23, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > 33)
+        {
+            transform.position = new Vector3(33, transform.position.y, transform.position.z);
+        }
+
+        //Keeping the player in bound along the z axis
+        if (transform.position.z < -6)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -6);
+        }
+        else if (transform.position.z > 51)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 51);
+        }
+
+
+        //moves the player forward basaed on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
         transform.Rotate(transform.up * turnSpeed * horizontalInput * Time.deltaTime);
-        //rotates the car based on horizontal input
+        //rotates the player based on horizontal input
     }
 
 }
