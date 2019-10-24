029c686074e37b47bdf3d84e8da73ecfa0263a72
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-10-24 10:30:26 -0500

    Added crosshair for aim and added new gun camera for raycasting

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera mainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hasHit;
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hasHit, range))
+        {
+            Debug.Log(hasHit.transform.name);
+        }
+    }
+}
