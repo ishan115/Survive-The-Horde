9895091084347dc5567593ef551687dea1de7eed
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-10-26 13:58:14 -0500

    Updated MainScene

---
+++ Assets/Scripts/Collision_Detection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Collision_Detection : MonoBehaviour
+{
+    [SerializeField] GameObject zombiePrefabs;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody shoot = zombiePrefabs.GetComponent<Rigidbody>();
+        FollowPlayer front = zombiePrefabs.GetComponent<FollowPlayer>();
+
+        front.MoveSpeed = 0;
+
+        Vector3 direction = Quaternion.Euler(0, 15, 30) * transform.forward * -10;
+
+        shoot.AddForce(direction * 5, ForceMode.Impulse);
+
+        Destroy(gameObject, 5);
+        Destroy(other.gameObject);
+    }
+}
