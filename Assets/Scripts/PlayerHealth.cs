05162b9b5bde6391c8e882e96a8464b240c01944
Ishan Dutta <ali3nator5000@gmail.com>
2019-11-03 13:07:05 -0600

    Implemented Health System

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] GameObject[] zombiePrefabs;
+
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite Heart_fill;
+    public Sprite Heart_blank;
+
+    private void Update()
+    {
+        healthSystem();
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        int zombieIndex = Random.Range(0, zombiePrefabs.Length);
+        Rigidbody shoot = zombiePrefabs[zombieIndex].GetComponent<Rigidbody>();
+
+        if (other.gameObject.CompareTag("Zombies"))
+        {
+            health = -1;
+            Destroy(other.gameObject);
+
+            if (health == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    public void healthSystem()
+    {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = Heart_fill;
+            }
+            else
+            {
+                hearts[i].sprite = Heart_blank;
+            }
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
