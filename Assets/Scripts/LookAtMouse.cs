897f50823981a3ea73e7d4ce88a77cd1ecd82329
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-11-25 15:05:33 -0600

    Tweaked aiming, still needs work

--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LookAtMouse : MonoBehaviour
 {
+    public GameObject projectileBullet;
+    [SerializeField]
+    private float bulletSpeed = 40f;
+
+    NavMeshAgent agent;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDir = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        Vector3 lookDir = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+
         Ray mouseRay = Camera.main.ScreenPointToRay(lookDir);
-        float midPoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
+
+        float midPoint = (transform.position - Camera.main.transform.position).magnitude;
         transform.LookAt(mouseRay.origin + mouseRay.direction * midPoint);
+
+        //Reference taken from: https://www.youtube.com/watch?v=DtT8Jnz56DY
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 Spawnpos = new Vector3(transform.position.x, -8.5f, transform.position.z);
+
+            GameObject instBullet = Instantiate(projectileBullet, Spawnpos, Quaternion.identity) as GameObject;
+            Rigidbody instRigidBullet = instBullet.GetComponent<Rigidbody>();
+            instRigidBullet.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,12 +14,12 @@ public class PlayerController : MonoBehaviour
 
     // Start is called before the first frame update
     public float speed = 20.0f;
-    public float bulletSpeed = 40f;
     public float turnSpeed = 200.0f;
     public float horizontalInput;
     public float forwardInput;
 
-    public GameObject projectileBullet;
+    public float rotationSpeed;
+    public float angularSpeed;
 
     public Animator animator;
 
@@ -28,13 +28,13 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        //horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
@@ -64,29 +64,15 @@ public class PlayerController : MonoBehaviour
         //moves the player forward basaed on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-        transform.Rotate(transform.up * turnSpeed * horizontalInput * Time.deltaTime);
-        //rotates the player based on horizontal input
 
-        //Reference taken from: https://www.youtube.com/watch?v=DtT8Jnz56DY
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Vector3 Spawnpos = new Vector3(transform.position.x, -8.5f, transform.position.z);
-
-            GameObject instBullet = Instantiate(projectileBullet, Spawnpos, Quaternion.identity) as GameObject;
-            Rigidbody instRigidBullet = instBullet.GetComponent<Rigidbody>();
-            instRigidBullet.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
-        }
-
-        //mouse aiming
-        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
     }
 
-    //public void FixedUpdate()
-    //{
-    //    Vector3 lookDir = mousePos - rb.position;
-    //    float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-    //    rb.rotation = angle;
-    //}
+    private void FixedUpdate()
+    {
+        rotationSpeed = rb.velocity.magnitude;
+        angularSpeed = rb.angularVelocity.magnitude / (Mathf.PI * 2);
 
+        rb.angularVelocity = new Vector3(0, Mathf.PI * 2, 0);
+    }
 }
